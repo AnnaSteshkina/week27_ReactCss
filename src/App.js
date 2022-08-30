93836ac04f60ebe6b2a7f0bd8a4923c5c757080f
AnnaSte <steshkinanna@gmail.com>
2022-08-30 15:42:17 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ import Rate from './components/rates.jsx';
 function App() {
   return (
     <div className="App">
-      <Rate className = "rate1"
+      <Rate
+      color = "#ed665c"
       price = {300}
       speed = {10}
       details = "Объем включенного трафика неограничен"
+      isHighlighted = {false}
       />
-      <Rate className = "rate2"
+      <Rate
+      color = "#00a8c9"
       price = {450}
       speed = {50}
       details = "Объем включенного трафика неограничен"
+      isHighlighted = {false}
       />
-      <Rate className = "rate3"
+      <Rate 
+      color = "#00c94a"
       price = {550}
       speed = {100}
       details = "Объем включенного трафика неограничен"
+      isHighlighted = {true}
       />
-      <Rate className = "rate4"
+      <Rate 
+      color = "#030000"
       price = {1000}
       speed = {200}
       details = "Объем включенного трафика неограничен"
+      isHighlighted = {false}
       />
     </div>
   );
--- src/components/rates.jsx
+++ src/components/rates.jsx
@@ -1,19 +1,115 @@
-import './rates.css';
+import styled from 'styled-components';
+import React from 'react';
+import { useState } from 'react';
+
+
+const DivWrapper = styled.div`
+    width: ${props => props.isHighlighted ? '18%' : '15%'};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border: 1px solid #ffffff;
+`;
+
+const DivUpper = styled.div`
+    width: 100%;
+    height: ${props => props.isHighlighted ? '60px' : '50px'};
+    display: flex;   
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+    color: #ffffff;
+
+    background-color: ${props => props.color};
+    border-bottom: 1px solid #ffffff;
+`;
+
+const DivMain = styled.div`
+    width: 100%;
+    height: ${props => props.isHighlighted ? '110px' : '100px'};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-bottom: 1px solid #ffffff;
+    background-color: ${props => props.color};
+    opacity: 0.8;
+`;
+
+const DivMainWrapper = styled.div`
+    height: 70%;
+    display: flex;
+    flex-direction: row;
+`;
+
+const TextCurrency = styled.span`
+    align-self: flex-start;
+    color: #ffffff;
+`;
+
+const MainPrice = styled.span`
+    align-self: center;
+    font-weight: bold;
+    font-size: 2rem;
+
+    color: #ffffff;
+`;
+
+const TextMonth = styled.span`
+    align-self: flex-end;
+    color: #ffffff;
+`;
+
+const DivSpeed = styled.div`
+    width: 100%;
+    height: ${props => props.isHighlighted ? '40px' : '30px'};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 0.8rem;
+    color: #636363;
+
+    background-color: #f2f0f0;
+    border-bottom: 1px solid #ffffff;
+`;
+
+const DivLower = styled.div`
+    width: 100%;
+    height: ${props => props.isHighlighted ? '60px' : '50px'};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 0.9rem;
+    color: #636363;
+
+    background-color: #e3e3e3;
+`;
 
 function Rate(props){
+const [selected, setSelected] = useState(false);
+
+const handleSelected = () => {
+    setSelected(!selected);
+}
+
     return(
-        <div className='wrapper'>
-            <div className='upper-level'>Безлимитный {props.price}</div>
-            <div className='main-level'>
-                <div className='wrapper-main-level'>
-                    <span className='text-currency'>руб</span>
-                    <span className='main-price'>{props.price}</span>
-                    <span className='text-month'>/мес</span>
-                </div>
-            </div>
-            <div className='speed'>до {props.speed} Мбит/с</div>
-            <div className='lower-level'>{props.details}</div>
-        </div>
+        <DivWrapper {...props} onClick={handleSelected}>
+            {selected ? "Выбрано" : ""}
+            <DivUpper  {...props}>Безлимитный {props.price}</DivUpper>
+            <DivMain  {...props}>
+                <DivMainWrapper>
+                    <TextCurrency>руб</TextCurrency>
+                    <MainPrice>{props.price}</MainPrice>
+                    <TextMonth>/мес</TextMonth>
+                </DivMainWrapper>
+            </DivMain>
+            <DivSpeed {...props}>до {props.speed} Мбит/с</DivSpeed>
+            <DivLower {...props}>{props.details}</DivLower>
+        </DivWrapper>
     )
 }
 export default Rate;
